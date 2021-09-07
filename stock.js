47c1bc6924e1ec85e3c133db740c28e58736fc49
Manoj Asarsa <manojasarsa7611@gmail.com>
2021-09-07 07:33:57 +0530

    fix: Validated input

--- stock.js
+++ stock.js
@@ -4,20 +4,19 @@ const currentPrice = document.querySelector("#current-price");
 const stockBtn = document.querySelector("#stock-btn");
 const outputEl = document.querySelector("#outputEl");
 
-console.log("hi")
+
 function calculateProfitAndLoss(initial, quantity, current) {
+
     if (initial > current) {
         var loss = (initial-current) * quantity;
         var lossPercentage = ((loss/initial) * 100).toFixed(2);
-
         outputEl.innerText = `Hey, The loss is ${loss} and the percent is ${lossPercentage}%. 
         Tip: You should know when to get out before getting in!`;
         outputEl.style.color = "red";
     }
-    else if (current > initial) {
+    else if (initial < current) {
         var profit = (current- initial) * quantity;
         var profitPercentage = ((profit/initial) * 100).toFixed(2);
-        
         outputEl.innerText = `Hey, The profit is ${profit} and the percent is ${profitPercentage}%. 
         Tip : Profit booked > Profit in books!`;
         outputEl.style.color = "green";
@@ -30,15 +29,15 @@ function calculateProfitAndLoss(initial, quantity, current) {
 }
 
 function submitHandler() {
-    var ip = initialPrice.value;
-    console.log(ip)
-    var qty = stockQuantity.value;
-    var curr = currentPrice.value;
+    var ip = Number(initialPrice.value);
+    var qty = Number(stockQuantity.value);
+    var curr = Number(currentPrice.value);
+
     if ( ip > 0 && qty > 0 && curr > 0) {
         calculateProfitAndLoss(ip,qty,curr);
     }
     else {
-        outputEl.innerText = "Please enter positive values."
+        outputEl.innerText = "Please enter valid values."
     }
     
 }
